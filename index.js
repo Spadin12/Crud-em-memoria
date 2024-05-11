1678b54da2293e0628ca9c2fc57b998bfadc5ce7
Spadin12 <133693739+Spadin12@users.noreply.github.com>
2024-05-11 18:00:18 +0000

    Endpoint Read By ID '[GET] /personagem/:id' criado

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ app.get('/', function (req, res) {
 })
 
 const lista = ['java','kotlin','Android']
-
+//              0       1       2
 app.get('/personagem',function (req, res) {
     res.send (lista)
 })
+// Endpoint Read By ID [GET] /personagem/:id
+app.get('/personagem/:id', function(req, res) {
+    const id = req.params.id
+
+    const item = lista [id - 1]
+    res.send(item)
+}
+
+)
 app.listen(3000)
