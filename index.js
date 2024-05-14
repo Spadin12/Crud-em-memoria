82ca6e0e3a6d6951c919e551e4acea9cc9260c55
Spadin12 <133693739+Spadin12@users.noreply.github.com>
2024-05-14 22:18:34 +0000

    Implementação do Create '[POST] /personagem'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 
 app.get('/', function (req, res) {
@@ -16,7 +17,21 @@ app.get('/personagem/:id', function(req, res) {
 
     const item = lista [id - 1]
     res.send(item)
-}
+})
 
-)
+app.use(express.json())
+
+app.post('/personagem', function(req, res) {
+
+const body = req.body
+
+console.log (body)
+
+  const novoitem = body.nome 
+
+  lista.push(novoitem)
+
+  res.send('Item adicionado com sucesso:' + novoitem)
+
+})
 app.listen(3000)
